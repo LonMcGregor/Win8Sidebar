af404c4b34fed1f1934e79eea306801c47a43311
LonMcGregor <lemcgregor3@outlook.com>
2014-04-08 12:15:58 +0100

    More support for transitions
    2-state transitions now rely more on the position & right properties
    than display:none/block.
    This should make animation easier
    
    Added wider links to share menu
    
    Moved big blocks of javascript into their own .js file - should make
    things more modular and easy to import into a webpage

--- thing.js
+++ thing.js
@@ -11,3 +11,114 @@ sidebar.id = 'sidebarX';
 var htmlContent = 'Sidebar Injection Area';
 sidebar.innerHTML = htmlContent;
 bodyTag.appendChild(sidebar);
+
+
+
+function setSize(){
+	var h = window.innerHeight;
+	sidebarDims(h);
+	sidebarAligns(h);
+}
+
+function sidebarDims(h){
+	var items = ["sidebar", "majorContainer", "sbContainer"];
+	for (var i = 0; i < 3; i++){
+		document.getElementById(items[i]).style.height=(""+h+"px");
+		
+		document.getElementById(items[i]).style.position="absolute";
+		document.getElementById(items[i]).style.right="0px";
+		document.getElementById(items[i]).style.top="0px";
+	}
+}
+function sidebarAligns(h){
+	document.getElementById('linkContainer').style.position="relative";
+	
+	var containerHeight = document.getElementById('linkContainer').clientHeight;
+	
+	var loc = ((h/2)-containerHeight/2);
+	document.getElementById('linkContainer').style.top=(""+loc+"px");
+}
+
+
+function showSideBody(i){
+	var toggle = document.getElementById('sb'+i).toggle;
+
+	for(var j = 1; j < 6; j++){
+		moveElementIn(j);
+	}
+
+	if (toggle===0){
+		document.getElementById('sbContainer').style.display="block";
+		moveElementOut(i);
+	} else {
+		moveElementIn(i);
+		document.getElementById('sbContainer').style.display="none";
+	}
+}
+
+function moveElementIn(i){
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='40px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='0px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-40px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-80px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-120px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-160px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-200px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-240px';
+	}, 400);
+
+	document.getElementById('sb'+i).toggle = 0;
+}
+
+function moveElementOut(i){
+	document.getElementById('sb'+i).toggle = 1;
+	
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-240px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-200px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-160px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-120px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-80px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='-40px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='0px';
+	}, 400);
+	setTimeout(function() {
+		document.getElementById('sb'+i).style.right='40px';
+	}, 400);
+}
+
+function doSearch(event){
+	var keyCode =  ('which' in event) ? event.which : event.keyCode;
+	if (keyCode===13){
+		window.location="http://duckduckgo.com/?q="+document.getElementById('searchbox').value;
+	}
+}
+
+
